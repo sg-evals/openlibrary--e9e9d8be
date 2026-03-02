178b7e1b802bf775be05a428e6933fee1499b99d
sg-evals <benchmarks@sourcegraph.com>
2026-03-02 18:19:06 +0000

    Mirror of internetarchive/openlibrary at e9e9d8be33f09cd487b905f339ec3e76ad75e0bb
    Upstream: https://github.com/internetarchive/openlibrary
    Ref: e9e9d8be33f09cd487b905f339ec3e76ad75e0bb

---
+++ openlibrary/actions.py
@@ -0,0 +1,12 @@
+"""Custom OL Actions.
+"""
+
+import infogami
+import sys
+
+
+@infogami.action
+def runmain(modulename, *args):
+    print("run_main", modulename, sys.argv)
+    mod = __import__(modulename, globals(), locals(), modulename.split("."))
+    mod.main(*args)
